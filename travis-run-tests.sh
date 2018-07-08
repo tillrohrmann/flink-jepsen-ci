5fa62eef30a8ed384e3290034a7ef53c2d596037
gyao <gary@data-artisans.com>
2018-07-08 16:20:23 +0200

    Enable building Flink.

--- travis-run-tests.sh
+++ travis-run-tests.sh
@@ -3,14 +3,15 @@
 # Get test sources and build Flink job
 git clone --depth 50 --branch=FLINK-9004 https://github.com/GJL/flink.git
 cd flink/flink-end-to-end-tests/flink-datastream-allround-test
+git remote add upstream https://github.com/apache/flink.git && git fetch upstream master && git rebase FETCH_HEAD
 mvn ${MAVEN_COMPILE_OPTIONS} clean install -DskipTests -Dfast
 cp target/DataStreamAllroundTestProgram.jar ../../flink-jepsen/bin/
 cd ../../../
 
 # Run tests
 while sleep 9m; do echo "Still running ($SECONDS seconds)"; done &
-#- travis_wait 35 make run EXTRA_VARS="flink_url=\"https://s3.eu-central-1.amazonaws.com/flink-dists-204087123/flink-${TRAVIS_BUILD_NUMBER}.tgz\""
-make run EXTRA_VARS="test_suite=${TEST_SUITE}"
+make run EXTRA_VARS="flink_url=\"https://s3.eu-central-1.amazonaws.com/flink-dists-204087123/flink-${TRAVIS_BUILD_NUMBER}.tgz\" test_suite=${TEST_SUITE}"
+#make run EXTRA_VARS="test_suite=${TEST_SUITE}"
 testrc=$?
 kill %1
 
