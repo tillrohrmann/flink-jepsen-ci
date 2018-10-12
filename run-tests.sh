5f4dcefbfae171d5dee3cc1a092a2c5ade8f643a
gyao <gary@data-artisans.com>
2018-10-12 17:33:21 +0200

    Enable mesos-session and standalone-session tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,58 +1,84 @@
 #!/usr/bin/env bash
-################################################################################
-#  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-# limitations under the License.
-################################################################################
 
 set -euo pipefail
 
-common_jepsen_args+=(--ha-storage-dir hdfs:///flink
---job-jar bin/DataStreamAllroundTestProgram.jar
---job-args "--environment.parallelism 10 --state_backend.checkpoint_directory hdfs:///checkpoints --state_backend rocks --state_backend.rocks.incremental true"
---nodes-file ~/nodes
---tarball ${2}
---username admin
---ssh-private-key ~/.ssh/id_rsa)
+test_iterations=${1}
+tarball=${2}
+test_suite=${3}
+jepsen_args=()
+
+function init_jepsen_args {
+	jepsen_args=(--ha-storage-dir hdfs:///flink
+	--job-jar bin/DataStreamAllroundTestProgram.jar
+	--job-args "--environment.parallelism ${1} --state_backend.checkpoint_directory hdfs:///checkpoints --state_backend rocks --state_backend.rocks.incremental true"
+	--nodes-file ~/nodes
+	--tarball ${tarball}
+	--username admin
+	--ssh-private-key ~/.ssh/id_rsa)
+}
+
+function run_yarn_session_tests {
+	init_jepsen_args 10
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode yarn-session
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode yarn-session
+	lein run test "${jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode yarn-session
+}
+
+function run_yarn_job_tests {
+	init_jepsen_args 10
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode yarn-job
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode yarn-job
+	lein run test "${jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode yarn-job
+}
+
+function run_yarn_job_kill_tm_tests {
+	init_jepsen_args 10
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-single-task-manager --deployment-mode yarn-job
+}
+
+function run_mesos_session_tests {
+	init_jepsen_args 10
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode mesos-session
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode mesos-session
+	lein run test "${jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode mesos-session
+}
+
+function run_standalone_session_tests {
+	init_jepsen_args 3
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode standalone-session
+	lein run test "${jepsen_args[@]}" --nemesis-gen kill-job-managers --client-gen cancel-job --deployment-mode standalone-session
+}
 
 for i in $(seq 1 ${1})
 do
-	echo "Executing run #${i} of ${1}"
-	case $3 in
+	echo "Executing run #${i} of ${test_iterations}"
+	case ${test_suite} in
 		yarn-session)
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode yarn-session
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode yarn-session
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode yarn-session
+			run_yarn_session_tests
 			;;
 		yarn-job)
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode yarn-job
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode yarn-job
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode yarn-job
+			run_yarn_job_tests
 			;;
 		yarn-job-kill-tm)
-			lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-single-task-manager --deployment-mode yarn-job
+			run_yarn_job_kill_tm_tests
 			;;
-		# mesos-session)
-		#     lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --deployment-mode mesos-session
-		#     lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --deployment-mode mesos-session
-		#     lein run test "${common_jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --deployment-mode mesos-session
-		#     ;;
-	    ?)
-	        echo "Unknown test suite: $3"
-	        exit 1
-	        ;;
+		mesos-session)
+			run_mesos_session_tests
+			;;
+		standalone-session)
+			run_standalone_session_tests
+			;;
+		all)
+			run_yarn_session_tests
+			run_yarn_job_tests
+			run_yarn_job_kill_tm_tests
+			run_mesos_session_tests
+			run_standalone_session_tests
+			;;
+		*)
+			echo "Unknown test suite: ${test_suite}"
+			exit 1
+			;;
 	esac
 	echo
 done
